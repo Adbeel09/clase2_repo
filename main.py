399dedaca0ca25a2d451622f6a722864665acb10
Adbeel <adb.rodriguez@duocuc.cl>
2025-06-13 15:37:10 -0400

    agg Suma

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-print("Hola 1")
-print("Hola 2")
-print("Hola 3")
-print("Hola 4")
-print("Hola 4")
-print("Hola 4")
-print("Hola 4")
-print("Hola 4")
-print("Hola 4")
-print("Hola 4")
+while True:
+    print("Calculadora")
+    print("1. Sumar")
+    print("2. Restar")
+    print("3. Multiplicar")
+    print("4. Dividir")
+    print("5. Salir")
+
+    opcion = int(input("Ingrese una opcion: "))
+    num1 = int(input("Ingrese el primer numero: "))
+    num2 = int(input("Ingrese el segundo numero: "))
+    
+    if opcion == 1:
+        resultado = num1 + num2
+        print(resultado)
+    elif opcion == 5:
+        break
